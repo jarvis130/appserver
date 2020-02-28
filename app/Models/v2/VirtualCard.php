1a0b7f93a7655153d508bd94aef8aa179d27bd52
ptjobs <ptjobs@protonmail.com>
2020-02-28 10:09:10 +0800

    虚拟卡使用

--- app/Models/v2/VirtualCard.php
+++ app/Models/v2/VirtualCard.php
@@ -78,43 +78,4 @@ class VirtualCard extends BaseModel
 
         return self::formatBody();
     }
-
-    /**
-     * 加密函数
-     * @param   string  $str    加密前的字符串
-     * @param   string  $key    密钥
-     * @return  string  加密后的字符串
-     */
-    function card_encrypt($str, $key)
-    {
-        $coded = '';
-        $keylength = strlen($key);
-
-        for ($i = 0, $count = strlen($str); $i < $count; $i += $keylength)
-        {
-            $coded .= substr($str, $i, $keylength) ^ $key;
-        }
-
-        return str_replace('=', '', base64_encode($coded));
-    }
-
-    /**
-     * 解密函数
-     * @param   string  $str    加密后的字符串
-     * @param   string  $key    密钥
-     * @return  string  加密前的字符串
-     */
-    function decrypt($str, $key)
-    {
-        $coded = '';
-        $keylength = strlen($key);
-        $str = base64_decode($str);
-
-        for ($i = 0, $count = strlen($str); $i < $count; $i += $keylength)
-        {
-            $coded .= substr($str, $i, $keylength) ^ $key;
-        }
-
-        return $coded;
-    }
 }
