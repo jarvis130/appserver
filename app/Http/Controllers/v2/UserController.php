f78d5874fa0338344f27703ed41fbb02e26cf1e7
jarvis <41083104@qq.com>
2020-03-03 08:14:35 +0800

    解决前端code必须是6位的数字

--- app/Http/Controllers/v2/UserController.php
+++ app/Http/Controllers/v2/UserController.php
@@ -300,7 +300,7 @@ class UserController extends Controller
     {
         $rules = [
             'mobile'   => 'required|string',
-            'code'     => 'required|string|digits:6',
+            'code'     => 'string',
             'password' => 'string',
         ];
 
