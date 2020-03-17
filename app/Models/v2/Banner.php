0d031d64c6e8af5e2d9acc28a92348130dcb6184
ptjobs <ptjobs@protonmail.com>
2020-03-17 14:26:10 +0800

    banner信息从数据库获取

--- app/Models/v2/Banner.php
+++ app/Models/v2/Banner.php
@@ -10,19 +10,48 @@ class Banner extends BaseModel
     protected $table      = 'banners';
     public $timestamps = false;
     protected $guarded    = [];
+    protected $appends    = ['id', 'photo', 'link', 'title', 'type'];
+    protected $visible    = ['id', 'photo', 'link', 'title', 'type'];
 
     /**
      * 获取列表
-     * @param $scene int 场景值 null:全部 1：轮播图 2：广告位
+     * @param $scene int 场景值 1：轮播图 2：广告位
      * @return array
      */
-    public static function getList($scene = null)
+    public static function getList($scene)
     {
-        if($scene){
-            $data = self::where('scene', $scene)->get()->toArray();
+        $data = self::where('scene', $scene)->get()->toArray();
+
+        if($scene == 2){
+            return self::formatBody(['ads' => $data]);
         }else{
-            $data = self::all()->toArray();
+            return self::formatBody(['banners' => $data]);
         }
-        return self::formatBody(['banners' => $data]);
+
+    }
+
+    public function getIdAttribute()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getPhotoAttribute()
+    {
+        return formatPhoto($this->attributes['src']);
+    }
+
+    public function getLinkAttribute()
+    {
+        return $this->attributes['url'];
+    }
+
+    public function getTitleAttribute()
+    {
+        return $this->attributes['text'];
+    }
+
+    public function getTypeAttribute()
+    {
+        return $this->attributes['type'];
     }
 }
