767d31273ea306e3cd1ee73f81587dfe58fc2895
ptjobs <ptjobs@protonmail.com>
2020-03-17 13:46:04 +0800

    banner信息从数据库获取

--- app/Models/v2/Banner.php
+++ app/Models/v2/Banner.php
@@ -6,30 +6,25 @@ use App\Models\BaseModel;
 
 class Banner extends BaseModel
 {
-    public static function getList()
+    protected $connection = 'shop';
+    protected $table      = 'banners';
+    public $timestamps = false;
+    protected $guarded    = [];
+    protected $appends    = ['id', 'src', 'url', 'text', 'type'];
+    protected $visible    = ['id', 'src', 'url', 'text', 'type'];
+
+    /**
+     * 获取列表
+     * @param $scene int 场景值 null:全部 1：轮播图 2：广告位
+     * @return array
+     */
+    public static function getList($scene = null)
     {
-        $data = [];
-        $file = @file_get_contents(config('app.shop_url').'/data/flash_data.xml');
-        if (strlen($file) > 0) {
-            $data = self::get_flash_xml($file);
+        if($scene){
+            $data = self::where('scene', $scene)->toArray();
+        }else{
+            $data = self::all()->toArray();
         }
         return self::formatBody(['banners' => $data]);
-    }
-
-    private static function get_flash_xml($file)
-    {
-        $flashdb = array();
-
-        // 兼容v2.7.0及以前版本
-        if (!preg_match_all('/item_url="([^"]+)"\slink="([^"]*)"\stext="([^"]*)"\ssort="([^"]*)"\stype="([^"]*)"/', $file, $t, PREG_SET_ORDER)) {
-            preg_match_all('/item_url="([^"]+)"\slink="([^"]*)"\stext="([^"]*)"\stype="([^"]*)"/', $file, $t, PREG_SET_ORDER);
-        }
-        if (!empty($t)) {
-            foreach ($t as $key => $val) {
-                $val[4] = isset($val[4]) ? $val[4] : 0;
-                $flashdb[] = array('id' => $key, 'photo'=>formatPhoto($val[1]), 'link'=>$val[2],'title'=>$val[3],'sort'=>$val[4], 'type'=>$val[5]);
-            }
-        }
-        return $flashdb;
     }
 }
