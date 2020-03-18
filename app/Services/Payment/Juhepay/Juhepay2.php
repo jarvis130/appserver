3a5efb9541456b9337035b79c56be02c8968518b
ptjobs <ptjobs@protonmail.com>
2020-03-18 16:27:14 +0800

    聚合支付

--- app/Services/Payment/Juhepay/Juhepay2.php
+++ app/Services/Payment/Juhepay/Juhepay2.php
@@ -118,12 +118,10 @@ class Juhepay2
         $data = $this->encrypt($data, $juhepay_pay_public_key);
         $data = urlencode($data);
 
-        // 拼接商户号
-        $data .= '&merchNo=' . $juhepay_partner;
-
         //发送数据
         $param = array(
-            'data' => $data
+            'data' => $data,
+            'merchNo' => $juhepay_partner
         );
 
         //支付
