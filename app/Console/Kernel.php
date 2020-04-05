d880a96361d26edf90639323bbd017076d2c8ef6
ptjobs <ptjobs@protonmail.com>
2020-04-05 16:05:53 +0800

    视频图片压缩

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         'App\Console\Commands\ActivateLicense',
         'App\Console\Commands\DownloadPhoto',
+        'App\Console\Commands\DownloadVideoImage',
     ];
 
     /**
